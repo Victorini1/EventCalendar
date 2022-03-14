bcc8605a66a6d12b4b3dfeea776d9b3339bb14e1
Baeonetta <victor.m.kennedy@gmail.com>
2022-03-14 00:54:32 -0500

    Added layout to the planner using bootstrap

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+
+
+
+// 12 hours in a day and each need a seperate section to write memos in
+
+// add time blocks  that change color depending on the time
+// save the events 
+function saveEvent(event){
+    addEventListener.button("click", toLocal);
+}
+// Write things to the local drive
+function toLocal(){
+    localStorage.setItem.value
+}
+// Read from the local drive
+function fromLocal(){
+    localStorage.getItem.value
+}
